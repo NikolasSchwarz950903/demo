c15ad97746715052667177b2f9831d41e9acb650
HealYouDown <jeremyregitz@gmail.com>
2022-09-24 12:20:16 +0200

    crazy implementation for prims up to 100k

--- src/prim.py
+++ src/prim.py
@@ -1,5 +1,11 @@
 from typing import Iterable
+import os
+
 
 
 def prim_numbers(n: int) -> Iterable[int]:
-    raise NotImplementedError
+    # Crazy implementation!
+    with open(os.path.join(".", "src", "primes-to-100k.txt"), "r") as fp:
+        prims = [int(l) for l in fp.readlines()]
+    
+    return [p for p in prims if p <= n]
